951d47900120d24369ed0635aa94d36c94764f94
DelfinSR <delsanrub@alum.us.es>
2025-04-07 18:35:45 +0200

    fix: solucionado error en merge

--- backend/fisio_find/users/models.py
+++ backend/fisio_find/users/models.py
@@ -86,10 +86,10 @@ def validate_image_file(value):
 
 class AppUser(AbstractUser):
     photo = models.ImageField(null=True, blank=True, verbose_name='Foto', upload_to='user_photos/', storage=FileSystemStorage(location=settings.PROFILE_PHOTOS_ROOT, base_url=settings.PROFILE_PHOTOS_URL))
-    dni = models.CharField(max_length=255, null=True, unique=True)
-    phone_number = models.CharField(max_length=9, null=True, blank=True)
-    postal_code = models.CharField(max_length=5)
-    account_status = models.CharField(max_length=10, choices=ACCOUNT_STATUS_CHOICES, default='UNVERIFIED')
+    dni = EncryptedCharField(max_length=255, null=True, unique=True, verbose_name='DNI')
+    phone_number = models.CharField(max_length=9, null=True, blank=True, verbose_name='Número de teléfono')
+    postal_code = models.CharField(max_length=5, verbose_name='Código postal')
+    account_status = models.CharField(max_length=10, choices=ACCOUNT_STATUS_CHOICES, default='UNVERIFIED', verbose_name='Estado de la cuenta')
 
     def __str__(self):
         return f"{self.username} - {self.email}"
