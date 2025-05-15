b4ec443c8fd14ed563c7a7fcab3dccee4d61a8f9
DaniFdezCab <danfercab@alum.us.es>
2025-05-15 16:23:27 +0200

    fix: Botón de saldo en perfil de fisioterapeuta
    Co-authored-by: fracapgar1 <franciiscocg@users.noreply.github.com>

--- frontend/app/physio-management/profile/page.tsx
+++ frontend/app/physio-management/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Camera, Plus, Trash2, Edit, Save, StarIcon, Film, Bell, BicepsFlexed, Lock, X, ClipboardList } from 'lucide-react';
+import { Camera, Plus, Trash2, Edit, Save, StarIcon, Film, Bell, BicepsFlexed, Lock, X, ClipboardList,Wallet, Banknote } from 'lucide-react';
 import ScheduleCalendar from "@/components/ui/ScheduleCalendar";
 import { getApiBaseUrl } from "@/utils/api";
 import { GradientButton } from "@/components/ui/gradient-button";
@@ -1861,6 +1861,31 @@ const FisioProfile = () => {
                     Valorar ahora
                   </GradientButton>
                 )}
+              </div>
+            </div>
+
+             {/* Balance */}
+            <div className="bg-white rounded-2xl shadow-xl overflow-hidden mb-6">
+              <div className="px-6 py-4 bg-gradient-to-r from-teal-500 to-blue-600">
+                <h2 className="text-xl font-bold text-white flex items-center">
+                  <Wallet  className="mr-2" size={20} />
+                  Balance
+                </h2>
+              </div>
+
+              <div className="p-6">
+                <p className="text-gray-600 mb-4">
+                  En este apartado podra ver el saldo de su cuenta y los ingresos obtenidos con fisiofind
+                </p>
+                <Link href={`/physio-management/balance`} passHref>
+                <GradientButton
+                    variant="create"
+                    className="w-full py-2 px-4 font-medium rounded-xl flex items-center justify-center gap-2"
+                >
+                  <Banknote size={16} />
+                    Ver Ingresos
+                  </GradientButton>
+                  </Link>
               </div>
             </div>
             
